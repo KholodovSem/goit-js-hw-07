5ddfeab3e04c220226652bcbf3e7b867ca065cb1
Kholodov Sem <kholodovsem@gmail.com>
2022-07-04 13:57:46 +0300

    Выполнил ДЗ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// Переменные
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsMarkup = itemsMarkup(galleryItems);
 
-console.log(galleryItems);
+// Рендеринг разметки
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+// Слушатели событий
+galleryRef.addEventListener("click", onClickTakeLargeImg);
+
+galleryRef.addEventListener("click", openModalWindow);
+
+// Функции колл-бэки
+function itemsMarkup(object) {
+  return object
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+let largeImgUrl = "";
+
+function onClickTakeLargeImg(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  largeImgUrl = event.target.dataset.source;
+  console.log(event.target.dataset.source);
+}
+
+let modalWindow = "";
+
+function openModalWindow() {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  modalWindow = basicLightbox.create(`<img  src="${largeImgUrl}">`);
+  modalWindow.show();
+}
+
+document.addEventListener("keydown", closeModalWindowOnKey);
+function closeModalWindowOnKey(event) {
+  if (event.code === "Escape") {
+    modalWindow.close();
+    modalWindow = "";
+  }
+  console.log(event.code);
+}
+
+document.addEventListener("keydown", removeEventKeyUp);
+function removeEventKeyUp() {
+  if (modalWindow === "") {
+    document.removeEventListener("keydown", closeModalWindowOnKey);
+  } else {
+    document.addEventListener("keydown", closeModalWindowOnKey);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Переменные
+const galleryRef = document.querySelector(".gallery");
+const galleryItemsMarkup = itemsMarkup(galleryItems);
+
+// Рендеринг разметки
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+//Слушатели событий
+galleryRef.addEventListener("click", openModalWindow);
+
+// Функции колл-бэки
+function itemsMarkup(object) {
+  return object
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const modalWindow = new SimpleLightbox(".gallery a", {
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: "250",
+});
+console.log(modalWindow);
+
+function openModalWindow(event) {
+  event.preventDefault();
+}
